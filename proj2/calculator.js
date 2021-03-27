121e470afe56d2554087eb7e75ac039236f7fa24
Edward Luna <mister.luna@berkeley.edu>
2021-03-26 18:47:06 -0700

    [Proj 2] Added click color changing

--- proj2/calculator.js
+++ proj2/calculator.js
@@ -1,3 +1,13 @@
-window.addEventListener("load", function(){
-    // ....
+function buttonClick(button) { 
+    console.log(button);
+}
+
+function init() {
+    document.querySelector(".calc-buttons").addEventListener("click", function (event) {
+        buttonClick(event.target);
+    });
+}
+
+window.addEventListener("load", function () {
+    init()
 });
