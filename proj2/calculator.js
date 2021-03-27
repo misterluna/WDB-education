588ff00b1aaa6913771c4d62bbee0db2d9e1095f
Edward Luna <mister.luna@berkeley.edu>
2021-03-26 19:05:52 -0700

    [Proj 2] Start implementing operations

--- proj2/calculator.js
+++ proj2/calculator.js
@@ -1,9 +1,29 @@
+function performMath() {
+    // TODO
+    console.log("Implement operation!");
+}
+
 function buttonClick(button) {
-    const operator = button.classList.contains("right");
+    const operator = button.classList.contains("operator");
     const value = button.innerText;
     const screen = document.getElementById("screen");
     if (operator) {
-        
+        switch (value) {
+            case "C":
+                console.log("Implement operation!");
+                // TODO
+                break;
+            case "=":
+                console.log("Implement operation!");
+                // TODO
+                break;
+            case "←":
+                console.log("Implement operation!");
+                // TODO
+                break;
+            default:
+                performMath(value);
+        }
     } else {
         let currentValue = parseInt(screen.innerText);
         screen.innerHTML = 10 * currentValue + parseInt(value);
